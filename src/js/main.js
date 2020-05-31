224e9a00ebb00a70e606539f1a08f513f307d428
AkselDjun <akseldjun@gmail.com>
2020-05-31 19:41:21 +0300

    library architecture

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+import "./lib/lib";
+import $ from "./lib/lib";
+
+$("div").hide();//скрывает все элементы div
+$("div").show();//показывает все элементы div скрытые методом hide
+$(".active").toggle();
